fc043e4511cc3b9a4c9a3a19a97cb68c6f42af50
Conor Mac Amhlaoibh <conormccauley1999@gmail.com>
2021-03-20 10:14:34 +0000

    Added gitignore, C++ header and C++ template

---
+++ template.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int INF = 1<<29;
+const int MOD = 1000000007;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    // ...
+
+    return 0;
+}
